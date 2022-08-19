444824075a720d6e44dd5c6fc02be53c40056ed4
Tim Yang <timologize@gmail.com>
2022-08-19 17:20:52 -0600

    first commit

---
+++ src/com/chess/engine/pieces/Pawn.java
@@ -0,0 +1,82 @@
+package com.chess.engine.pieces;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
+import com.chess.engine.board.Move;
+import com.chess.engine.board.Move.MajorMove;
+import com.google.common.collect.ImmutableList;
+
+public class Pawn extends Piece {
+    private final int[] CANDIDATE_MOVE_COORDINATES = {8, 7, 9, 16};
+
+    public Pawn(final int piecePosition, final Alliance pieceAlliance) {
+        super(PieceType.PAWN, piecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(final Board board) {
+        final List<Move> legalMoves = new ArrayList<>();
+
+        for(final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATES) {
+            final int candidateDestinationCoordinate = this.piecePosition + (this.getPieceAlliance().getDirection() * currentCandidateOffset);
+
+            if(!BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+                continue;
+            }
+            
+            if (currentCandidateOffset == 8 && !board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
+                legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+            }
+            else if (currentCandidateOffset == 16 && this.isFirstMove() && 
+                    (BoardUtils.SECOND_ROW[this.piecePosition] && this.getPieceAlliance().isBlack()) || 
+                    (BoardUtils.SEVENTH_ROW[this.piecePosition] && this.getPieceAlliance().isWhite())) {
+                final int behindCandidateDestinationCoordinate = this.piecePosition + (this.pieceAlliance.getDirection() * 8);
+
+                if (!board.getTile(behindCandidateDestinationCoordinate).isTileOccupied() &&
+                    !board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
+                        legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                }
+            }
+            else if(currentCandidateOffset == 7 && 
+                   !((BoardUtils.EIGHTH_COLUMN[this.piecePosition] && this.getPieceAlliance().isWhite()) ||
+                    (BoardUtils.FIRST_COLUMN[this.piecePosition] && this.pieceAlliance.isBlack()))) {
+                if(board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
+                    final Piece pieceOnCandidate = board.getTile(candidateDestinationCoordinate).getPiece();
+                    
+                    if(this.pieceAlliance != pieceOnCandidate.getPieceAlliance()) {
+                        legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                    }
+                }
+            }
+            else if(currentCandidateOffset == 9 && 
+                   !((BoardUtils.FIRST_COLUMN[this.piecePosition] && this.getPieceAlliance().isWhite()) ||
+                    (BoardUtils.EIGHTH_COLUMN[this.piecePosition] && this.pieceAlliance.isBlack()))) {
+                if(board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
+                    final Piece pieceOnCandidate = board.getTile(candidateDestinationCoordinate).getPiece();
+                    
+                    if(this.pieceAlliance != pieceOnCandidate.getPieceAlliance()) {
+                        legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                    }
+                }
+            }
+        }
+
+        return ImmutableList.copyOf(legalMoves);
+    }
+
+    @Override
+    public Pawn movePiece(final Move move) {
+        return new Pawn(move.getDestinationCoordinate(), move.getMovedPiece().getPieceAlliance());
+    }
+
+    @Override
+    public String toString() {
+        return PieceType.PAWN.toString();
+    }
+    
+}
---
+++ src/com/chess/gui/Table.java
@@ -0,0 +1,98 @@
+package com.chess.gui;
+
+import javax.swing.*;
+
+import com.chess.engine.board.BoardUtils;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Table {
+    private final JFrame gameFrame;
+    private final BoardPanel boardPanel;
+    private final static Dimension OUTER_FRAME_DIMENSION = new Dimension(600,600);
+    private final static Dimension BOARD_PANEL_DIMENSION = new Dimension(400,500);
+    private final static Dimension TILE_PANEL_DIMENSION = new Dimension(10,10);
+    private final static Color lightTileColor = Color.decode("#FFFACD");
+    private final static Color darkTileColor = Color.decode("#593E1A");
+
+    public Table() {
+        this.gameFrame = new JFrame("JChess");
+        final JMenuBar tableMenuBar = createTableMenuBar();
+        this.gameFrame.setJMenuBar(tableMenuBar);
+        this.gameFrame.setSize(OUTER_FRAME_DIMENSION);
+         this.boardPanel = new BoardPanel();
+        this.gameFrame.add(this.boardPanel, BorderLayout.CENTER);
+        this.gameFrame.setVisible(true); 
+    }
+
+    private JMenuBar createTableMenuBar() {
+        final JMenuBar tableMenuBar = new JMenuBar();
+        tableMenuBar.add(createFileMenu());
+        return tableMenuBar;
+    }
+
+    private JMenu createFileMenu() {
+        final JMenu fileMenu = new JMenu("File");
+        
+        final JMenuItem openPGN = new JMenuItem("Load PGN File");
+        openPGN.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("open up that pgn file!");
+            }
+        });
+        fileMenu.add(openPGN);
+
+        final JMenuItem exitMenuItem = new JMenuItem("Exit");
+        exitMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }        
+        });
+        fileMenu.add(exitMenuItem);
+
+        return fileMenu;
+    }
+
+    private class BoardPanel extends JPanel {
+        final List<TilePanel> boardTiles;
+
+        BoardPanel() {
+            super(new GridLayout(8,8));
+            this.boardTiles = new ArrayList<>();
+
+            for(int i = 0; i < BoardUtils.NUM_TILES; i++) {
+                final TilePanel tilePanel = new TilePanel(this, i);
+                this.boardTiles.add(tilePanel);
+                add(tilePanel);
+            }
+
+            setPreferredSize(BOARD_PANEL_DIMENSION);
+            validate();
+        }
+    }
+
+    private class TilePanel extends JPanel {
+        private final int tileId;
+
+        TilePanel(final BoardPanel boardPanel,
+                  final int tileId) {
+            super(new GridBagLayout());
+            this.tileId = tileId;
+            setPreferredSize(TILE_PANEL_DIMENSION);
+            assignTileColor();
+            validate();
+        }
+
+        private void assignTileColor() {
+            boolean isLight = ((tileId + tileId/8) % 2 == 0);
+            setBackground(isLight ? lightTileColor : darkTileColor);
+        }
+
+    }
+}
